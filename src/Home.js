2d1ff03e94b31e93de12c800b213e1c7a577f4ff
jessieward <jessica_ward25@milton.edu>
2024-12-03 12:31:28 -0500

    API

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,35 @@
 import React,{useEffect, useState} from 'react';
-import Colors from './Colors';
-import axois from 'axios';
+import Colors from './colors';
+import axios from 'axios';
 
 const Home=()=>{
-    // useEffect(()=>){
-    //     axios.get
-    // }
+    const[Data,setData]=useState({
+        Company: '',
+        Description: ''
+    })
+    const[colorsData,setColorsData]=useState([])
+    
+     useEffect(()=>{
+         axios.get('https://reqres.in/api/unknown')
+         .then(res=>{
+            console.log('Response from main API:',res)
+            console.log('Home Data: ', res.data.ad)
+             let companyData=res.data.ad;
+             setData({Company:"Dogs", Description:"Dogs"})
+             console.log('Colors Data: ', res.data.data)
+             setColorsData(res.data.data)
+               
+         })
+         .catch(err=>{
+             console.log(err);
+         })
+     },[])
 
     return(
         <>
-            <h1> This is the Home component</h1>
-            <Colors/>
+            <h1> {Data.Company}</h1>
+            <p>{Data.Description}</p>
+            <Colors data={colorsData}/>
         </>
     )
 }
